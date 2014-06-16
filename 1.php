a60bfeb662af7b70a1705b610a6713f17d70b09e
iamnoob <yeq@yandex.ru>
2014-06-16 12:33:03 +0400

    Delete 1.php

--- 1.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Content-Type: text/html; charset=utf-8");
-$name=$_POST['name'];
-$str = file_get_contents('http://vk.com/id'.$name);
-preg_match('/<title>(.*)<\/title>/s', $str, $m);
-
-echo $m[1];
