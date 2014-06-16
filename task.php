a7d5accda439e6929ad30cd4bb73601ea6e916b9
oleg.agafonov <yeq@yandex.ru>
2014-06-16 12:27:58 +0400

    second commit

--- task.php
+++ task.php
@@ -7,7 +7,6 @@ if($pos === false){
     echo "fail";
 }
 else{
-    $count = "1";
     $mystring = str_replace($findme, "", $mystring, $count);
     echo "Слово ЕБАЛ в строке найдено " . $count . " раз.";
 }
